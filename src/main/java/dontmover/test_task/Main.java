facce767b628ab2a8ba793db390f19dc7fa51d15
DonTMover <antantov624@gmail.com>
2022-12-11 20:47:50 +0300

    fixed что-то

--- src/main/java/dontmover/test_task/Main.java
+++ src/main/java/dontmover/test_task/Main.java
@@ -19,54 +19,80 @@ public class Main {
             items.add(new item(str[0],str[1]));
         }
 
-        for (item item : items) {
-            System.out.println("amount: "+item.getAmount());
-            System.out.println("id: "+item.getId());
-        }
-        for (item item : items) {
-            if (item.getId().equalsIgnoreCase("card")){
-                String amount = item.getAmount();
-                ResultSet resultSet = sql.AllQuery("cards");
-                while (resultSet.next()){
-                    String id = String.valueOf(resultSet.getInt("id"));
-                    if (id.equalsIgnoreCase(amount)){
-                        hascard=true;
-                        System.out.println("true");
-                    }else {
-                        System.out.println("FAKE CARD!!!");
-                        hascard=false;
-                    }
-                }
+//        for (item item : items) {
+//            System.out.println("amount: "+item.getAmount());
+//            System.out.println("id: "+item.getId());
+//        }
+//        for (item item : items) {
+//            if (item.getId().equalsIgnoreCase("card")){
+//                String amount = item.getAmount();
+//                ResultSet resultSet = sql.AllQuery("cards");
+//                while (resultSet.next()){
+//                    String id = String.valueOf(resultSet.getInt("id"));
+//                    if (id.equalsIgnoreCase(amount)){
+//                        hascard=true;
+//                        System.out.println("true card");
+//                    }else {
+//                        System.out.println("FAKE CARD!!!");
+//                        hascard=false;
+//                    }
+//                }
+//
+//            }else {
+//                System.out.println("false");
+//                hascard=false;
+//            }
+//        }
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//        ResultSet resultSet = sql.AllQuery("items");
+//        while(resultSet.next()){
+//            String id = String.valueOf(resultSet.getInt("id"));
+//            for (item item : items) {
+//                if (item.getId().equals(id)) {
+//                    if (id.equals("card")){
+//
+//                    }
+//
+//                    System.out.println("+");
+//                } else {
+//                    System.out.println("-");
+//                }
+//
+//            }
+//            System.out.printf("%s\n",id);
+//        }
 
-            }else {
-                System.out.println("false");
-                hascard=false;
-            }
-        }
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        ResultSet resultSet = sql.AllQuery("items");
-        while(resultSet.next()){
-            String id = String.valueOf(resultSet.getInt("id"));
+        ResultSet resultSet = sql.AllQuery("cards");
+
+        String id1 = "";
+        while (resultSet.next()){
             for (item item : items) {
-                if (item.getId().equals(id)) {
-                    if (id.equals("card")){
-
-                    }
-
-                    System.out.println("+");
-                } else {
-                    System.out.println("-");
+                if (item.getId().equals("card")){
+                    id1 = item.getAmount();
                 }
 
             }
-            System.out.printf("%s\n",id);
+            String id = String.valueOf(resultSet.getInt("id"));
+            if (id.equals(id1)) {
+                hascard = true;
+                System.out.println("true card!");
+            }
         }
+        resultSet.close();
+        ResultSet resultSet1 = sql.AllQuery("items");
+        while (resultSet1.next()){
+            for (item item : items) {
+                if(item.getId().equalsIgnoreCase(resultSet1.getString("id"))){
+                    System.out.println("SADASASD");
+                }
+                System.out.println(resultSet1.getString("id"));
 
-
+            }
+        }
 
 
 
